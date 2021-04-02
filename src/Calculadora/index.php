025527fc5c38308f13aa1986bb3418158a8077dc
Alexsandro Cristiano <acg.alexsandro@hotmail.com>
2021-04-01 23:51:22 -0300

    exercicio calculadora - formulario pronto

---
+++ src/Calculadora/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicio calculadora</title>
+</head>
+<body>
+    <form action="calculadora.php" method="get">
+        <label for="valorA">Valor A:</label>
+        <br>
+        <input type="text" name="valorA" id="valorA">
+        <br>
+        <label for="valorB">Valor B:</label>
+        <br>
+        <input type="text" name="valorB" id="valorB">
+        <br>
+        <input type="submit" value="enviar">
+    </form>
+</body>
+</html>
