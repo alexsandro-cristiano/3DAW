515c20c4e5a9c504ca05315a9f22cde85bae0e00
Alexsandro Cristiano <acg.alexsandro@hotmail.com>
2021-04-05 08:50:42 -0300

    Aplicando css no formulario

--- src/Calculadora/index.php
+++ src/Calculadora/index.php
@@ -5,18 +5,20 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Exercicio Somador</title>
+    <style>
+        label, input {
+            display:block;
+            margin-bottom: 10px;
+        }
+    </style>
 </head>
 <body>
     <h1>Somador</h1>
     <form action="calculador.php" method="get">
         <label for="valorA">Valor A:</label>
-        <br>
         <input type="text" name="valorA" id="valorA">
-        <br>
         <label for="valorB">Valor B:</label>
-        <br>
         <input type="text" name="valorB" id="valorB">
-        <br><br>
         <input type="submit" value="enviar">
     </form>
 </body>
