5bcd1d0176a27fe2c0f748415203a69df68dd4ea
Alexsandro Cristiano <75186371+alexsandro-cristiano@users.noreply.github.com>
2021-05-12 14:10:05 -0300

    Funcionalidade Listar Um - Com Erro

--- src/provas/av1/system/views/client/listar.php
+++ src/provas/av1/system/views/client/listar.php
@@ -29,7 +29,7 @@ $conexao->close();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../css/main.css">
     <link rel="stylesheet" href="../../css/cliente.css">
-    <title>CRUD Cliente | Alterar</title>
+    <title>CRUD Cliente | Listar Todos</title>
 </head>
 
 <body>
